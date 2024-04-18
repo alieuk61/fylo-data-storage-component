a1a115df512a7b6f4ba75163c487772aa5bdd9c9
Alieu Kamara <alieuk61@gmail.com>
2024-04-18 05:09:55 +0100

    second commit

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 const slider = document.querySelector('.slider');
+const sliderValue = slider.value
+const percentage = (sliderValue / 1000) * 100;
+console.log(slider.value, percentage)
 
+slider.style.background = `linear-gradient(to right, hsl(6, 100%, 80%) 0%, hsl(335, 100%, 65%) ${percentage}%, transparent ${percentage}%)`
